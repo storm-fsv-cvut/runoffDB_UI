5b7e35ad08f081bda1699b1b694f0b5d9cb28cfc
Pavel Prokop <pavel.prokop@ftonline.cz>
2019-03-24 23:54:41 +0100

    generate login form template

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+require('../bower_components/bootstrap/dist/css/bootstrap.min.css');
+require('../bower_components/font-awesome/css/font-awesome.min.css');
+require('../bower_components/Ionicons/css/ionicons.min.css');
+require('../bower_components/bootstrap/dist/css/bootstrap.min.css');
+require('../dist/css/AdminLTE.min.css');
+require('../dist/css/skins/skin-blue.css');
+require('../css/app.css');
+
+
+require('../bower_components/bootstrap/dist/js/bootstrap.min.js');
+require('../dist/js/adminlte.min.js');
+
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ppavel
+ * Date: 13.03.2019
+ * Time: 22:14
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController {
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index() {
+        return $this->render('default/index.html.twig');
+    }
+}
