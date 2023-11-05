bcd7c29e567e3abfaec1afeea3d6b3788c0fe6d6
Pavel Prokop <>
2023-11-05 21:18:01 +0100

    fix redirect

---
+++ src/EventSubscriber/AccessDeniedSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class AccessDeniedSubscriber implements EventSubscriberInterface
+{
+    private RequestStack $requestStack;
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(RequestStack $requestStack, UrlGeneratorInterface $urlGenerator) {
+        $this->requestStack = $requestStack;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => ['onKernelException', 2],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+
+        $exception = $event->getThrowable();
+        if (!$exception instanceof AccessDeniedException) {
+            return;
+        }
+
+        $locale = $this->requestStack->getCurrentRequest()!==null ? $this->requestStack->getCurrentRequest()->getLocale(
+        ) ?? $this->requestStack->getCurrentRequest()->getDefaultLocale() : null;
+
+        $homepage = $this->urlGenerator->generate('homepage', ['_locale'=>$locale], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $event->setResponse(new RedirectResponse($homepage));
+    }
+}
