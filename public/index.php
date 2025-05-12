51d50852baf2cbd2c2cbe24cfc845d225b9661b2
Pavel Prokop <>
2025-05-12 14:22:39 +0200

    up and running

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 ini_set('memory_limit','1024M');
 require dirname(__DIR__).'/config/bootstrap.php';
