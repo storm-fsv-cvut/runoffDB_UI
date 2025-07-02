7e5df8c72b3d2af2e0ab2c4489be38a3da093b88
Pavel Prokop <>
2025-07-02 22:17:26 +0200

    add sequence note

---
+++ src/Migrations/Version20250702200039.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250702200039 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            ALTER TABLE sequence ADD note_cz VARCHAR(255) DEFAULT NULL, ADD note_en VARCHAR(255) DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
